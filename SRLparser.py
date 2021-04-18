c017a03dc85fa2b4e29198046e97302a41c5acdd
andrei828 <andrei.liviu10@gmail.com>
2021-04-18 11:05:45 +0300

    fixed bug in closure calculation

--- SRLparser.py
+++ SRLparser.py
@@ -2,9 +2,15 @@ import copy
 from collections import deque
 
 beginNonTerminal = None
+terminals = set(['$'])
+nonTerminals = set()
+dfa = {}
 
 def readGrammatic():
+  global terminals
+  global nonTerminals
   global beginNonTerminal
+
   grammatic = {}
   with open('grammatic.txt', 'r') as inputFile:
     # for _ in range(2):
@@ -20,13 +26,21 @@ def readGrammatic():
       for item in tmp[2:]:
         if item == '|':
           grammatic[tmp[0]].append(currentValue[:])
+          nonTerminals.add(tmp[0])
+          terminals |= set(currentValue)
           currentValue = []
         else:
           currentValue.append(item)
+          terminals.add(item)
       try:
         grammatic[tmp[0]].append(currentValue[:])
+        terminals |= set(currentValue)
       except:
-        grammatic[tmp[0]]
+        pass
+
+    for item in nonTerminals:
+      if item in terminals:
+        terminals.remove(item)
 
     return grammatic
 
@@ -37,6 +51,41 @@ def printGrammatic(grammatic):
       print(" ".join(rule), end=' | ')
     print()
 
+def getFirst(grammatic, symbol, visited=set()):
+  if symbol not in grammatic:
+    return set([symbol])
+  else:
+    visited.add(symbol)
+    result = set()
+    for value in grammatic[symbol]:
+      if len(value) > 0:
+        if value[0] not in grammatic:
+          result.add(value[0])
+        elif value[0] not in visited:
+          result |= getFirst(grammatic, value[0], visited)
+    return result
+
+def first(grammatic):
+  return {item: getFirst(grammatic, item, set()) for item in grammatic}
+
+'''modify for epsilon if necesarry'''
+def follow(grammatic, first):
+  flw = {item: set() for item in grammatic}
+  flw[beginNonTerminal].add('$')
+  for key, values in grammatic.items():
+    for value in values:
+      for i in range(len(value) - 1):
+        if value[i] in grammatic:
+          if value[i + 1] in first:
+            flw[value[i]] |= first[value[i + 1]]
+          else:
+            flw[value[i]].add(value[i + 1])
+
+  for key, values in grammatic.items():
+    for value in values:
+      if value[-1] in flw:
+        flw[value[-1]] |= flw[key]
+  return flw  
 
 def buildFirstItem(grammatic, key):
   result = {key: []}
@@ -50,6 +99,7 @@ def buildFirstItem(grammatic, key):
 
 def closure(grammatic, item):
   while True:
+    
     itemSize = 0
     for val in item.values():
       itemSize += len(val)
@@ -70,7 +120,8 @@ def closure(grammatic, item):
               item[key].append(value)
         else:
           item[key] = dictionary[key]
-    # big potential problem with this criteria
+    
+    # big potential problem with this condition
     currentSize = 0
     for val in item.values():
       currentSize += len(val)
@@ -92,15 +143,33 @@ def goto(item, symbol):
             result[key] = [tmp]
   return result
 
+def setRules(grammatic):
+  result = {}
+  k = 0
+  for key, matrix in grammatic.items():
+    for value in matrix:
+      result[k] = {key: value}
+      k += 1
+  return result
+
 def isItemPresent(currentItem, items):
   found = True
   searchList = []
   currentList = []
-  for itemDict in items.values():
+  # print('-----------------------')
+  # print(currentItem)
+  # print('000009090909090')
+  # print(items)
+  helper = {}
+  # print(items)
+  for masterKey, itemDict in items.items():
     for key in itemDict:
       for value in itemDict[key]:
         searchList.append([key] + value)
-
+        if masterKey not in helper:
+          helper[masterKey] = [[key] + value]
+        else:
+          helper[masterKey].append([key] + value)
   for currentKeys in currentItem:
     for values in currentItem[currentKeys]:
       currentList.append([currentKeys] + values)
@@ -108,10 +177,24 @@ def isItemPresent(currentItem, items):
   for current in currentList:
     if current not in searchList:
       found = False
-
+  
+  if found == True:
+    for itemNum, values in helper.items():
+      total = 0
+      for current in currentList:
+        if current in values:
+          total += 1
+      if total == len(currentList):
+        return itemNum
+    # print(helper)
+  # print(found)
+  # print(searchList)
+  # print(helper)
+  # print('-----------------------')
   return found        
 
 def constructItems(grammatic):
+  global dfa
   firstItem = buildFirstItem(grammatic, beginNonTerminal)
   firstItem = closure(grammatic, firstItem)
   items = {0: firstItem}
@@ -123,17 +206,40 @@ def constructItems(grammatic):
   while True:
     itemsListSize = len(items)
     tmp = []
+    beforeK = k
     for num, dictionary in items.items():
       for key in dictionary:
         for value in dictionary[key]:
           for i in range(len(value) - 1):
             if value[i] == '.':
+              
               newItem = closure(grammatic, goto(items[num], value[i + 1]))
-              if not isItemPresent(newItem, items):
-                print('GOTO(', num, value[i + 1], ') ', newItem)
-                if newItem not in tmp:
+              # print(num, value[i + 1], isItemPresent(newItem, items))
+              isConnected = isItemPresent(newItem, items)
+              if not isConnected and newItem not in tmp:
+                  # print('GOTO(', num, value[i + 1], ') ', newItem)
+                  dfa[(num, value[i + 1])] = k
                   tmp.append(copy.deepcopy(newItem))
+              else:
+                dfa[(num, value[i + 1])] = isConnected
 
+
+  # itemsListSize = len(items)
+  # tmp = []
+  # beforeK = k
+  # for num, dictionary in items.items():
+  #   for key in dictionary:
+  #     for value in dictionary[key]:
+  #       for i in range(len(value) - 1):
+  #         if value[i] == '.':
+  #           newItem = closure(grammatic, goto(items[num], value[i + 1]))
+            
+  #           if not isItemPresent(newItem, items):
+  #             if newItem not in tmp:
+  #               # print('GOTO(', num, value[i + 1], ') ', newItem)
+  #               dfa[(num, value[i + 1])] = k
+  #               tmp.append(copy.deepcopy(newItem))
+    k = beforeK
     for i in tmp:
       items[k] = i
       k += 1
@@ -141,11 +247,49 @@ def constructItems(grammatic):
     if itemsListSize == len(items):
       return items
 
-  # return firstItem
+def printGotoTable(table):
+  print('\t', '\t'.join(nonTerminals))
+  for i in range(len(table)):
+    print(i, end='\t')
+    for val in table[i].values():
+      print(val, end='\t')
+    print()
 
+def printActionTable(table):
+  print('\t', '\t'.join(terminals))
+  for i in range(len(table)):
+    print(i, end='\t')
+    for val in table[i].values():
+      print(val, end='\t')
+    print()
+
+def buildParsingTable(first, follow, items, dfa, terminals, nonTerminals):
+  gotoTable = [{i: 0 for i in nonTerminals} for _ in range(len(items))]
+  actionTable = [{i: 0 for i in terminals} for _ in range(len(items))]
+  for tupl, value in dfa.items():
+    if tupl[1] in terminals:
+      actionTable[tupl[0]][tupl[1]] = value
+  printActionTable(actionTable)
+  printGotoTable(gotoTable)
+  return gotoTable
 
 grammatic = readGrammatic()
 # print(grammatic)
 printGrammatic(grammatic)
+# print(setRules(grammatic))
+
+# print(terminals) 
 print()
-print(constructItems(grammatic))
+items = constructItems(grammatic)
+# print(items)
+print('DFA:')
+print(dfa)
+print()
+# for key, value in dfa.items():
+#   print(value, items[value])
+# print()
+
+firstSet = first(grammatic)
+followSet = follow(grammatic, firstSet)
+
+print(buildParsingTable(firstSet, followSet, items, dfa, terminals, nonTerminals))
